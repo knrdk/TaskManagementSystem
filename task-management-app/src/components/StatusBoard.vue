69eba1fd12866193d3a1fba1ce61558b2d935fee
Konrad Kopciuch <konrad1991@gmail.com>
2018-09-28 10:29:30 +0200

    Initialize todo web app in vue (#1)
    * created vue app
    * simple todo list implemented

---
+++ task-management-app/src/components/StatusBoard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="status-board">
+      <div class="new-todo">
+          <input v-model="newItem"/>
+          <button @click="addItem()" :disabled="!newItem">Add</button>
+      </div>
+    <ul>
+        <li v-for="(item, index) in items" :key="`todo-${index}`">
+            {{item}} <a @click="deleteItem(index)" class="delete-todo-link">[Delete]</a>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: ['First item', 'Second item'],
+      newItem: '',
+    };
+  },
+  methods: {
+    deleteItem(index) {
+      this.items.splice(index, 1);
+    },
+    addItem() {
+      this.items.push(this.newItem);
+      this.newItem = '';
+    },
+  },
+};
+</script>
+
+<style scoped>
+.delete-todo-link {
+    color: red;
+}
+</style>
---
+++ task-management-app/src/router.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './components/HomePage.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/statusBoard',
+      name: 'statusBoard',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './components/StatusBoard.vue'),
+    },
+  ],
+});
