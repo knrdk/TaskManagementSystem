d22375cc9bff96b860f7851624ff6b9f61a05cef
knrdk <konrad1991@gmail.com>
2018-09-28 11:07:03 +0200

    implemented drag&drop of todos

--- task-management-app/src/components/StatusBoard.vue
+++ task-management-app/src/components/StatusBoard.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="status-board">
+    <div class="todos">
+      <Container @drop="onDrop(items, $event)">
+      <Draggable v-for="(item, index) in items" :key="`todo-${index}`">
+        <div class="todo-item">
+            {{item}} <a @click="deleteItem(index)" class="delete-todo-link">[Delete]</a>
+        </div>
+      </Draggable>
+      </Container>
+    </div>
+    <div class="footer">
       <div class="new-todo">
           <input v-model="newItem"/>
           <button @click="addItem()" :disabled="!newItem">Add</button>
       </div>
-    <ul>
-        <li v-for="(item, index) in items" :key="`todo-${index}`">
-            {{item}} <a @click="deleteItem(index)" class="delete-todo-link">[Delete]</a>
-        </li>
-    </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import { Container, Draggable } from 'vue-smooth-dnd';
+
 export default {
+  components: { Container, Draggable },
   data() {
     return {
-      items: ['First item', 'Second item'],
+      items: ['First item', 'Second item', 'Third item'],
       newItem: '',
     };
   },
@@ -28,12 +37,30 @@ export default {
       this.items.push(this.newItem);
       this.newItem = '';
     },
+    onDrop(source, dropResult) {
+      const { removedIndex, addedIndex } = dropResult;
+
+      if (removedIndex !== null && addedIndex !== null) {
+        const itemToAdd = source.splice(removedIndex, 1)[0];
+        source.splice(addedIndex, 0, itemToAdd);
+      }
+    },
   },
 };
 </script>
 
 <style scoped>
 .delete-todo-link {
-    color: red;
+  color: red;
+}
+.todo-item {
+  border-style: solid;
+  border-width: 1px;
+  margin: 5px;
+  padding: 5px;
+}
+.status-board {
+  width: 250px;
+  background-color: aliceblue;
 }
 </style>
