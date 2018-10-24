1183938833ff687f3cc4bfbeadb187d5cf13a440
knrdk <konrad1991@gmail.com>
2018-10-24 21:22:07 +0200

    disable https redirection in api

--- task-management-api/TaskManagement.Api/Startup.cs
+++ task-management-api/TaskManagement.Api/Startup.cs
@@ -40,7 +40,7 @@ namespace TaskManagement.Api
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
